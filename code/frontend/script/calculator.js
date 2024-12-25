18cd7b2038319743456cf59ba5fd029e39bcac5e
gfloriano11 <gflorianodev@gmail.com>
2024-12-25 16:21:13 -0300

    delete: organize the script to good pratices

--- code/frontend/script/calculator.js
+++ code/frontend/script/calculator.js
@@ -2,46 +2,22 @@ const display = document.querySelector('#display');
 const operators = document.querySelectorAll('.operator');
 const result = document.querySelector('#result')
 const numbers = document.querySelectorAll('.number');
-let numberOne = document.querySelector('.number_one'); 
 
-if(!numberOne){
-    let clicked = 0;
-    
-    numbers.forEach((number) => {
+numbers.forEach((number) => {
 
-        number.addEventListener('click', () => {
+    number.addEventListener('click', () => {
+        
+        let num = number.dataset.value;
+
+        const input = document.createElement('p');
             
-            clicked++
-            
-            console.log('clique nº: ', clicked);
-            
-            let num = number.dataset.value;
-            
-            if(clicked <= 10){
-                
-                const input = document.createElement('p');
-                
-                // const numberOne = document.createElement('div');
-                // numberOne.classList.add('number_one');
-                
-                // const numberTwo = document.createElement('div');
-                // numberTwo.classList.add('number_two');
-                
-                input.classList.add('input_number');
-                input.innerText = num;
-                
-                // result.appendChild(numberOne);
-                result.appendChild(input);
-                
-                // console.log(opTeste);
-                // result.appendChild(opTeste);
-            } else {
-                window.alert('Número máximo atingido!');
-            }
-            
-        });
+        input.classList.add('input_number');
+        input.innerText = num;
+        
+        result.appendChild(input);
+        
     });
-}
+});
 
 operators.forEach((operator) => {
     
@@ -49,27 +25,24 @@ operators.forEach((operator) => {
 
         const sign = operator.dataset.value;
 
-        // console.log(sign)
 
         if(sign === '='){
             result.innerHTML = '';
-            clicked = 0;
-            
+        } else{
+
+            const signContainer = document.createElement('div');
+            signContainer.classList.add('math_sign');
+    
+            const opTeste = document.createElement('p');
+            opTeste.innerText = sign;
+    
+            result.appendChild(signContainer);
+            signContainer.appendChild(opTeste);
         }
-
-        const signContainer = document.createElement('div');
-        signContainer.classList.add('math_sign');
-
-        const opTeste = document.createElement('p');
-        opTeste.innerText = sign;
-
-        result.appendChild(signContainer);
-        signContainer.appendChild(opTeste);
 
         let teste = document.querySelector('.math_sign');
 
-        if(teste){
-        } else {
+        if(!teste){
             console.log('não existe sinal')
         }
 
